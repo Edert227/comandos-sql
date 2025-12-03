bbd1cdc5901765d3a76e61d3daf2c338bc094454
Edert Guevara <guevaraedert0@gmail.com>
2025-12-03 16:33:25 -0500

    se crea archivo sql

---
+++ ejemplos_comando_sql.sql
@@ -0,0 +1,9 @@
+-- DDL
+-- CREATE DATABASE crea una base de datos
+
+ CREATE DATABASE  testSBD;
+ 
+ CREATE DATABASE  bdBorrar;
+ 
+ -- DROP DATABASE elimina una base de datos
+ DROP DATABASE bdBorrar;
