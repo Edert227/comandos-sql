0301fffdc7a78386e72e749543a5a8601a3035ab
Edert Guevara <guevaraedert0@gmail.com>
2025-12-03 17:51:09 -0500

    se suben  los  ejemplos ejemplos faltantes

--- ejemplos_comando_sql.sql
+++ ejemplos_comando_sql.sql
@@ -9,5 +9,34 @@
  DROP DATABASE bdBorrar;
 
  
- 
+ use testSBD;
+ CREATE TABLE PERSON (
+ per_id int ,
+ per_last_name varchar (255) ,
+ per_firs_name varchar (255) ,
+ per_adrdres varchar(255)
+ );
+TRUNCATE TABLE PERSON;
+DROP TABLE PERSON;
+CREATE TABLE equipos(
+equipo_id int ,
+realm varchar(255),
+chels varchar(255),
+dormt varchar(255)
+);
+ALTER TABLE equipos
+ADD livp varchar(255);
+
+ALTER TABLE equipos
+CHANGE COLUMN chels chelsea VARCHAR(255);
+
+ALTER TABLE equipos
+MODIFY COLUMN realm VARCHAR(15);
+
+
+
+
+
+
+
  
